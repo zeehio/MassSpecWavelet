b6ef563e82d2d3297121ba3393ca2fd252267ff5
Sergio Oller <sergioller@gmail.com>
2022-04-01 21:34:46 +0200

    Register C routines

--- src/convolve2.c
+++ src/convolve2.c
@@ -1,12 +1,14 @@
-     #include <R.h>
-     #include <Rdefines.h>
-     
+#include <R.h>
+#include <Rdefines.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
      SEXP convolve2(SEXP a, SEXP b)
      {
        int i, j, na, nb, nab;
        double *xa, *xb, *xab;
        SEXP ab;
-     
+
        PROTECT(a = AS_NUMERIC(a));
        PROTECT(b = AS_NUMERIC(b));
        na = LENGTH(a); nb = LENGTH(b); nab = na + nb - 1;
@@ -19,4 +21,17 @@
        UNPROTECT(3);
        return(ab);
      }
+
+
+
+static const R_CallMethodDef CallEntries[] = {
+    {"convolve2", (DL_FUNC) &convolve2, 2},
+    {NULL, NULL, 0}
+};
+
+void R_init_MassSpecWavelet(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
 

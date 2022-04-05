dc1d706dc0f717637ebce85fd2220a3263ad081e
Sergio Oller <sergioller@gmail.com>
2022-04-05 12:36:45 +0200

    Remove internal sav.gol function (and related ones)

--- R/c.R
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------
-## code added by Steffen Neumann 
-# ----------------------------------------------------------------------
-
-conv <- function(a, b) {
-  .Call("convolve2", as.double(a), as.double(b), PACKAGE = "MassSpecWavelet")
-}  

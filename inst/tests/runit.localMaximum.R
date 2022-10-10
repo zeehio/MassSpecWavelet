46030b5dcf9a492184301d2f54710c7a89e6a2ad
Sergio Oller <sergioller@gmail.com>
2022-10-10 05:42:30 +0200

    Add unit test to verify that the new implementation gives identical results to the old one

--- inst/tests/runit.localMaximum.R
+++ inst/tests/runit.localMaximum.R
@@ -3,8 +3,7 @@ test01_localMaximum <- function() {
     checkEquals(localMaximum(c(1,2,3,4,2,1), winSize = 5), c(0,0,0,1,0,0), msg = "Check localMaximum on a short vector")
 }
 
-
-test02_checkCorrectness <- function() {
+test02_checkIdenticalResults_classic_vs_faster <- function() {
     set.seed(5413L)
     winSizes <- c(5, 31, 301)
     xlengths <- c(20, 200, 2000, 20000)
@@ -14,11 +13,37 @@ test02_checkCorrectness <- function() {
             for (simulation in simulations) {
                 x <- round(10*runif(xlength), 1)*10
                 options(MassSpecWavelet.localMaximum.algorithm = "classic")
-                localmax_classic <- which(localMaximum(x, winSize = winSize) > 0)
+                localmax_classic <- localMaximum(x, winSize = winSize)
+                options(MassSpecWavelet.localMaximum.algorithm = "faster")
+                localmax_faster <- localMaximum(x, winSize = winSize)
+                checkEquals(
+                    localmax_classic,
+                    localmax_faster,
+                    msg = "faster and classic do not give identical results"
+                )
+            }
+        }
+    }
+}
+
+
+test03_check_new_does_not_miss_maxima <- function() {
+    set.seed(5413L)
+    winSizes <- c(5, 31, 301)
+    xlengths <- c(20, 200, 2000, 20000)
+    simulations <- 1:20
+    for (winSize in winSizes) {
+        for (xlength in xlengths) {
+            for (simulation in simulations) {
+                x <- round(10*runif(xlength), 1)*10
+                options(MassSpecWavelet.localMaximum.algorithm = "faster")
+                localmax_faster <- localMaximum(x, winSize = winSize)
                 options(MassSpecWavelet.localMaximum.algorithm = "new")
-                localmax_new <- which(localMaximum(x, winSize = winSize) > 0)
-                missing <- setdiff(localmax_classic, localmax_new)
-                # Remove borders because classic has false positives in them
+                localmax_new <- localMaximum(x, winSize = winSize)
+                localmax_faster <- which(localmax_faster > 0)
+                localmax_new <- which(localmax_new > 0)
+                missing <- setdiff(localmax_faster, localmax_new)
+                # Remove borders because classic&faster have false positives in them
                 missing <- missing[-which(missing < winSize/2 | missing > (xlength - winSize/2))]
                 # Remove plateaus because we have another criteria
                 missing <- setdiff(missing, missing[x[missing] == x[missing + 1L]])
